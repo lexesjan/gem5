5b79d53bd1c9736ac021d5ef47dbede967bcde83
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2022-03-01 19:07:37 +0000

    arm-cortex-m4: Add basic counter implementation
    Change-Id: I2b1ed9b6b401f0162865cdab9e6cfbfa826de57c

--- src/arm-cortex-m4/dwt.cc
+++ src/arm-cortex-m4/dwt.cc
@@ -8,11 +8,24 @@ namespace ArmCortexM4
 {
 
 DWT::DWT(const gem5::ArmDWTParams &params)
-    : gem5::BasicPioDevice(params, 0x2000)
+    : gem5::BasicPioDevice(params, 0x2000),
+      control(0x40000001),
+      counters(DWT_NCNTRS)
 {
 }
 
 DWT::~DWT() {}
+
+void
+DWT::incrementCounterValue(uint32_t registerAddr, uint32_t value)
+{
+    assert(isValidCounterRegisterAddr(registerAddr));
+
+    DPRINTF(ArmCortexM4DWT, "Incrementing counter %#x by value %d\n",
+        registerAddr, value);
+    uint32_t id = DWT::registerAddrToId(registerAddr);
+    counters[id] += value;
+}
 
 gem5::Tick
 DWT::read(gem5::PacketPtr pkt)
@@ -20,10 +33,29 @@ DWT::read(gem5::PacketPtr pkt)
     assert(pkt->getAddr() >= pioAddr && pkt->getAddr() < pioAddr + pioSize);
     assert(pkt->getSize() == 4);
 
-    gem5::Addr daddr = pkt->getAddr() - pioAddr;
+    gem5::Addr registerAddr = pkt->getAddr() - pioAddr;
 
-    DPRINTF(ArmCortexM4DWT, "Reading from DWT at offset: %#x\n", daddr);
-    pkt->setLE<uint32_t>(0xFAFAFAFA);
+    DPRINTF(ArmCortexM4DWT, "Reading from DWT at offset: %#x\n", registerAddr);
+    switch(registerAddr) {
+        case DWT_CTRL: {
+            pkt->setLE<uint32_t>(control);
+            break;
+        }
+        case DWT_CYCCNT ... DWT_LSUCNT: {
+            uint32_t id = DWT::registerAddrToId(registerAddr);
+            pkt->setLE<uint32_t>(counters[id]);
+            break;
+        }
+        case DWT_PCSR ... DWT_FUNCTION3:
+        case PID4 ... CID3: {
+            warn("Unimplemented DWT register read: %#x\n", registerAddr);
+            pkt->setLE<uint32_t>(0);
+            break;
+        }
+        default: {
+            panic("Unexpected DWT register read: %#x\n", registerAddr);
+        }
+    }
 
     pkt->makeAtomicResponse();
 
@@ -36,9 +68,28 @@ DWT::write(gem5::PacketPtr pkt)
     assert(pkt->getAddr() >= pioAddr && pkt->getAddr() < pioAddr + pioSize);
     assert(pkt->getSize() == 4);
 
-    gem5::Addr daddr = pkt->getAddr() - pioAddr;
+    gem5::Addr registerAddr = pkt->getAddr() - pioAddr;
 
-    DPRINTF(ArmCortexM4DWT, "Writing to DWT at offset: %#x\n", daddr);
+    DPRINTF(ArmCortexM4DWT, "Writing to DWT at offset: %#x\n", registerAddr);
+    switch(registerAddr) {
+        case DWT_CYCCNT ... DWT_LSUCNT: {
+            uint32_t id = DWT::registerAddrToId(registerAddr);
+            uint32_t data = pkt->getLE<uint32_t>();
+            counters[id] = data;
+            break;
+        }
+        case DWT_CTRL:
+        case DWT_PCSR ... DWT_FUNCTION3:
+        case PID4 ... CID3: {
+            warn("Unimplemented DWT register write: %#x\n", registerAddr);
+            pkt->setLE<uint32_t>(0);
+            break;
+        }
+        default: {
+            panic("Unexpected DWT register write: %#x\n", registerAddr);
+        }
+    }
+
     pkt->makeAtomicResponse();
 
     return pioDelay;
--- src/arm-cortex-m4/dwt.hh
+++ src/arm-cortex-m4/dwt.hh
@@ -1,6 +1,9 @@
 #ifndef __ARM_CORTEX_M4_DWT_HH__
 #define __ARM_CORTEX_M4_DWT_HH__
 
+#include <vector>
+
+#include "base/bitunion.hh"
 #include "dev/io_device.hh"
 #include "params/ArmDWT.hh"
 
@@ -13,8 +16,106 @@ class DWT: public gem5::BasicPioDevice
     DWT(const gem5::ArmDWTParams &params);
     ~DWT();
 
+    enum
+    {
+        DWT_CTRL      = 0x0,
+        DWT_CYCCNT    = 0x4,
+        DWT_CPICNT    = 0x8,
+        DWT_EXCCNT    = 0xC,
+        DWT_SLEEPCNT  = 0x10,
+        DWT_LSUCNT    = 0x14,
+        DWT_FOLDCNT   = 0x18,
+        DWT_PCSR      = 0x1C,
+        DWT_COMP0     = 0x20,
+        DWT_MASK0     = 0x24,
+        DWT_FUNCTION0 = 0x28,
+        DWT_COMP1     = 0x30,
+        DWT_MASK1     = 0x34,
+        DWT_FUNCTION1 = 0x38,
+        DWT_COMP2     = 0x40,
+        DWT_MASK2	    = 0x44,
+        DWT_FUNCTION2 = 0x48,
+        DWT_COMP3     = 0x50,
+        DWT_MASK3     = 0x54,
+        DWT_FUNCTION3 = 0x58,
+        PID4          = 0x1FD0,
+        PID5          = 0x1FD4,
+        PID6          = 0x1FD8,
+        PID7          = 0x1FDC,
+        PID0          = 0x1FE0,
+        PID1          = 0x1FE4,
+        PID2          = 0x1FE8,
+        PID3          = 0x1FEC,
+        CID0          = 0x1FF0,
+        CID1          = 0x1FF4,
+        CID2          = 0x1FF8,
+        CID3          = 0x1FFC,
+
+        DWT_NCNTRS     = 0x6,
+    };
+
+    BitUnion32(DWT_CTRL_t)
+        Bitfield<0>     CYCCNTENA;
+        Bitfield<4,1>   POSTPRESET;
+        Bitfield<8,5>   POSTINIT;
+        Bitfield<9>     CYCTAP;
+        Bitfield<11,10> SYNCTAP;
+        Bitfield<12>    PCSAMPLENA;
+        Bitfield<16>    EXCTRCENA;
+        Bitfield<17>    CPIEVTENA;
+        Bitfield<18>    EXCEVTENA;
+        Bitfield<19>    SLEEPEVTENA;
+        Bitfield<20>    LSUEVTENA;
+        Bitfield<21>    FOLDEVTENA;
+        Bitfield<22>    CYCEVTENA;
+        Bitfield<24>    NOPRFCNT;
+        Bitfield<25>    NOCYCCNT;
+        Bitfield<26>    NOEXTTRIG;
+        Bitfield<27>    NOTRCPKT;
+        Bitfield<31,28> NUMCOMP;
+    EndBitUnion(DWT_CTRL_t)
+
+    /**
+     * Increments a counter by the value
+     *
+     * @param offsetAddr ID of the counter.
+     * @param value Value to increment by.
+     */
+    void incrementCounterValue(uint32_t offsetAddr, uint32_t value);
+
+  private:
+    /** Control register */
+    DWT_CTRL_t control;
+
+    /** Current state of the counters */
+    std::vector<uint32_t> counters;
+
+    /**
+     * Checks if the counter register address is valid
+     *
+     * @param registerAddr Register address to check.
+     */
+    bool isValidCounterRegisterAddr(uint32_t registerAddr)
+    {
+        return registerAddr % 4 == 0 && registerAddr >= DWT_CYCCNT &&
+               registerAddr <= DWT_FOLDCNT;
+    }
+
+    /**
+     * Converts a register address to a counter ID
+     *
+     * @param registerAddr Offset address to convert.
+     */
+    uint32_t registerAddrToId(uint32_t registerAddr)
+    {
+      return registerAddr / static_cast<uint32_t>(DWT_NCNTRS);
+    }
+
+  // BasicPioDevice
+  public:
     /**
      * Handle a read to the device
+     *
      * @param pkt The memory request.
      * @param data Where to put the data.
      */
@@ -22,6 +123,7 @@ class DWT: public gem5::BasicPioDevice
 
     /**
      * Handle a write to the device
+     *
      * @param pkt The memory request.
      * @param data the data
      */
